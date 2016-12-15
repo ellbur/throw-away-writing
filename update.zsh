e12ec19600b36f14fc4910ca4b9357752716afd4
Owen Healy <owen@pulsetor.com>
2016-12-14 23:58:31 -0500

    Fix update script

--- update.zsh
+++ update.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
-rsync --progress -avzu -e ssh . ubuntu@ellbur.com:/var/www/html/throw-away-writing
+# http://askubuntu.com/questions/482916/rsync-exclude-hidden-files-doesnt-work
+rsync --progress -avzu -e ssh --exclude='.*' --exclude='.*/' --exclude='update.zsh' . ubuntu@ellbur.com:/var/www/html/throw-away-writing
 
